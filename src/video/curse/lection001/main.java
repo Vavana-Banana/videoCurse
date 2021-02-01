0acbca3779b3df525c4275cdaeac4b361586df01
Vavana-Banana <vladimir9vladimir9@gmail.com>
2021-02-01 22:32:40 +0300

    video curses

---
+++ src/video/curse/lection001/main.java
@@ -0,0 +1,19 @@
+package video.curse.lection001;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("                РУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда открытая мной.");
+        System.out.println("                                О. Хайям");
+        char c1 = 0654;
+        char c2 = 0655;
+        char c3 = 0695;
+        String s1 ;
+        s1 = c1;
+        System.out.println(c1);
+    }
+
+}
