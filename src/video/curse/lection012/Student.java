45aef30d59ee93032f18694f906e1754f2b963fa
Vavana-Banana <vladimir9vladimir9@gmail.com>
2021-03-20 20:37:06 +0300

    homework lesson 12 is done

---
+++ src/video/curse/lection012/Student.java
@@ -0,0 +1,51 @@
+package video.curse.lection012;
+
+public class Student {
+    String name;
+    int course;
+    double grade;
+
+    Student(String name, int course, double grade) {
+        this.name = name;
+        this.course = course;
+        this.grade = grade;
+
+    }
+
+    static class StudentTest {
+        void equalSt(Student st1, Student st2) {
+            if (st1.name == st2.name && st1.course == st2.course && st1.grade == st2.grade) {
+                System.out.println("Oni ravny");
+
+            } else System.out.println("Ony ne ravny");
+        }
+        void equalStDetail(Student st1, Student st2){
+
+            if (st1.name==st2.name){
+                if (st1.course==st2.course){
+                    if (st1.grade==st2.grade){
+                        System.out.println("Studenty ravny");
+                    }
+                    else System.out.println("Studenty ne ravny ocenki");
+                }
+                else System.out.println("Studenty ne ravny cursy");
+            }
+            else System.out.println("Studenty ne ravny imena");
+        }
+
+
+        public static void main(String[] args) {
+            Student st1 = new Student("Ivan", 4, 7.8);
+            Student st2 = new Student("Ivan", 4, 8.8);
+
+            StudentTest eq = new StudentTest();
+
+            eq.equalSt(st1, st2);
+
+            eq.equalStDetail(st1, st2);
+
+
+
+        }
+    }
+}
